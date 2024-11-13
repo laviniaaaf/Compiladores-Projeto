86f358aea8d90fa29073042d741ff85cc5e7e3b4
laviniaaaf <102470754+laviniaaaf@users.noreply.github.com>
2024-11-13 18:22:15 -0300

    Update tokens.h

--- tokens.h
+++ tokens.h
@@ -1,5 +1,13 @@
-#define IDENT 1
-#define INT 2
-#define PLUS 3
-#define ASSIGN 4
-#define NUMBER 5
+#ifndef TOKENS_H
+#define TOKENS_H
+
+#define T_KEYWORD    1
+#define T_IDENTIFIER 2
+#define T_NUMBER     3
+#define T_OPERATOR   4
+#define T_ASSIGN     5
+#define T_PAREN      6
+#define T_SEMICOLON  7
+#define T_STRING     8
+
+#endif
